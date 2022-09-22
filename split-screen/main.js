271a0454d099fa4409bcbaae9c324a89aa51d657
nick <vo.nick2686@gmail.com>
2022-09-22 14:13:07 -0700

    added new projects

---
+++ split-screen/main.js
@@ -0,0 +1,12 @@
+//Get elements from the DOM
+const wrapper = document.querySelector('.split-screen');
+const topLayer = document.querySelector('.left');
+
+//Add mousemove event
+wrapper.addEventListener('mousemove', function(e) {
+  /*Move top layer by setting its left 
+  position to the mouse position on the X axis*/
+  topLayer.style.width = e.clientX + 'px';
+}); 
+
+
